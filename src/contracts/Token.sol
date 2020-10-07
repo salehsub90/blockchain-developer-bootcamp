a94142552c2d9bbf5a9b4a60341c893ede843d84
Saleh Suboh <subohsaleh@gmail.com>
2020-10-07 16:19:07 -0400

    adding more modifications

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,13 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
-const privateKeys = process.env.PRIVATE_KEYS || "";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const privateKeys = [
+  "fed3f0dad02d361c283430738face5c19fdfe6bfb7df61d40a7d81e06ba64b6c",
+  "df3a781df22fde3901425398b39cab28e1c30abd30c8bd5632edd1ffe4c52574"
+];
+const INFURA_API_KEY = "e481184584ca4985a8bb96079d7c7320"; 
+//const privateKeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
   networks: {
@@ -14,8 +19,8 @@ module.exports = {
     kovan: {
       provider: function() {
         return new HDWalletProvider(
-          privateKeys.split(','),
-          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`
+          privateKeys,
+          `https://kovan.infura.io/v3/${INFURA_API_KEY}`
         )
       },
       gas: 5000000,
